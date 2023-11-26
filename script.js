431b3d97e7182ee1fa77ff39470fcfe8f9a17cc2
nikatopu <nikatopu@gmail.com>
2023-11-26 18:30:15 +0400

    REWORK
    I reworked the entire website to make it look a bit more modern styled

---
+++ script.js
@@ -0,0 +1,128 @@
+// List of objects with text and image properties
+const work_experience = [
+  {
+    title: "Social Media Manager at Career Home",
+    text: "I was in charge of handling multiple social media platforms, creating content, and carrying out plans to increase engagement and grow our online presence. Besides managing social media, I focused on building and maintaining customer relationships. I interacted with our audience, responding quickly to inquiries, comments, and messages to ensure high customer satisfaction and a positive brand image.<br><br>I also took on the responsibility of editing videos for social media. This included planning, editing, and producing engaging video content to showcase our products/services and connect with our audience effectively.<br><br>Additionally, I contributed to graphic design tasks as part of the overall content creation process. This involved creating visual elements for social media posts to maintain brand consistency and visual appeal across all platforms.",
+    image:
+      "https://scontent.ftbs6-2.fna.fbcdn.net/v/t39.30808-6/376672486_177598988690070_1869829048044851372_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=MmUjtyMU_PsAX92NoP8&_nc_ht=scontent.ftbs6-2.fna&oh=00_AfDraxnxP_ZVAmUfSyl0JvT9ew6GHhFvrfbRkTNqBylJJQ&oe=65655C66",
+    link: "https://www.facebook.com/profile.php?id=100093199086358",
+  },
+  {
+    title: "Sales & Marketing Associate at Iberieli",
+    text: "I developed and maintained strong relationships with business associates to ensure ongoing collaboration and satisfaction. In my role, I directly sold wine to customers, using persuasive communication and product knowledge to drive sales and meet customer needs effectively. I played a significant part in expanding the market reach of the family business by targeting and engaging new audiences through strategic marketing initiatives.<br><br>Moreover, I served as a representative of Iberieli at various festivals, acting as a brand ambassador. This involved showcasing company products at international events, including a London-based exhibition, where I successfully promoted and represented the brand to a broader audience.",
+    image:
+      "https://scontent.ftbs6-2.fna.fbcdn.net/v/t39.30808-1/263399615_4922209917797553_6286832800449964494_n.jpg?stp=dst-jpg_p320x320&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=lswIsTNkcCUAX_y8ucZ&_nc_ht=scontent.ftbs6-2.fna&oh=00_AfAeLkXazgiA2JXHNkT2YmiIMPneV7A0OvajGtawf45LEQ&oe=656650B5",
+    link: "https://www.facebook.com/iberieliwinerypage",
+  },
+];
+
+game_development = [
+  {
+    title: '"CUBICAL"',
+    text: "This game, CUBICAL, is a significant achievement in my collection of work. While I am proud of creating it, I recognize the challenges that came with combining puzzle and action elements. <br><br> CUBICAL started as a fun project created for entertainment, using a 2D Game Engine called GameMaker. The main goal was to make a visually interesting game that looks three-dimensional, even though it was made within the limitations of a 2D Game Engine. <br><br> During this project, I gained valuable skills in creating complex puzzles and improved my ability to make 2D art and sound effects. The project was a great learning experience that gave me deep insights into the complex world of game development. It also helped me understand programming in the broader context of the industry. <br><br> CUBICAL's diverse nature allowed me to handle the challenges of both game development and programming, contributing to my overall growth and professionalism. The combination of creative problem-solving, artistic expression, and technical expertise displayed in this project reflects my commitment to continually improving and adapting in dynamic professional settings.",
+    image:
+      "https://img.itch.zone/aW1hZ2UvMTQ4OTIyNC84NjgyMDQzLnBuZw==/original/Rer00i.png",
+    link: "https://nicolaeus.itch.io/cubical",
+  },
+  {
+    title: '"Quit Horsing Around!"',
+    text: "While I may not take particular pride in it, I acknowledge the merits of the game, QUIT HORSING AROUND!, given the circumstances of its development.<br><br>Originating from a GameJam that utilized the 2D Game Engine, GameMaker, QUIT HORSING AROUND! was conceived within the constraints and thematic parameters of the event. I skillfully integrated the phrase 'Quit Horsing Around' into the core concept of this fast-paced, action-oriented game.<br><br>The progression of this project has significantly contributed to my professional development, especially in terms of employability. The process of creating QUIT HORSING AROUND! taught me to navigate effectively under stressful conditions and tight timelines, fostering the acquisition of invaluable skills. This endeavor serves as a testament to my adaptability and capacity to deliver results even in challenging circumstances, attributes that enhance my suitability as a potential employee in diverse work environments.",
+    image:
+      "https://img.itch.zone/aW1nLzEwNjYzOTE1LnBuZw==/315x250%23c/vnwy9p.png",
+    link: "https://nicolaeus.itch.io/quit-horsing-around",
+  },
+  {
+    title: '"The Robodventure"',
+    text: "This specific project, THE ROBODVENTURE, holds a significant place among my professional achievements. Despite my pride in its creation, a notable drawback is the excessive use of dialogue. <br><br> Conceived for a Christmas GameJam utilizing the 2D Game Engine, GameMaker, THE ROBODVENTURE was developed as part of a distinctive challenge. In this GameJam, I took on the role of a 'GameDev Santa', assigned the task of crafting a personalized game for a designated recipient. The individual provided preferences through a letter, guiding the ideation and subsequent development of the game concept.<br><br>The process of creating THE ROBODVENTURE played a crucial role in enhancing my qualifications as a potential candidate. It compelled me to explore uncharted aspects of game development, fostering innovation within the constraints of a limited time frame—merely one month. This experience not only highlighted my ability to adapt and deliver under challenging conditions but also demonstrated my capability to tailor creative solutions to meet specific preferences, a skill that is highly applicable in various professional contexts.",
+    image: "https://img.itch.zone/aW1nLzc3NDAxODIucG5n/original/eEWsz8.png",
+    link: "https://nicolaeus.itch.io/the-robodventure",
+  },
+];
+
+creative_writing = [
+  {
+    title: "Poem - Let's Fly Far",
+    text: "I played a role in creating the poetic element of this animation, with my sister, Mariam Topuridze, handling the animation, and my brother, Ioane Topuridze, composing the musical accompaniment.<br><br>The inspiration for the poem stemmed from a straightforward video that highlighted the distinctions between two types of caterpillars. These caterpillars, though leading similar lives, undergo diverse transformations during their development.<br><br>Participating in the creation of this poem proved to be a mentally engaging pursuit. While the act of writing itself was conventional, the true fascination lay in the collaborative effort with my siblings to produce a collective work. Additionally, I took on the responsibility of translating the poem into English, a task I believe I carried out proficiently. <br><br>This experience has enriched my skill set and professional attributes in various ways. The collaboration with my siblings underscored my ability for teamwork and cooperation, highlighting my effectiveness in contributing to a collaborative creative process. The translation task further showcased my linguistic proficiency and attention to detail. Collectively, this multifaceted involvement demonstrates my adaptability, creativity, and meticulous approach to tasks, qualities that significantly enhance my capabilities as a work applicant.",
+    //Yes I'm using a discord photo stfu
+    image:
+      "https://media.discordapp.net/attachments/435802394375225346/1178340298410172447/oh.png?ex=6575c9eb&is=656354eb&hm=32b5d7d027a8ce939f06c4559e7f76957ce1ce0fdc5d9b76b02c1a5e7d30b558&=&format=webp&width=927&height=663",
+    link: "https://nikatopu.github.io/topuportfolio/fly%20far.mp4",
+  },
+];
+
+// Function to create content blocks based on the list
+function createContentBlocks(list) {
+  const container = document.getElementById("content-container");
+  container.innerHTML = ""; // Clear previous content
+
+  // Loop through the list and create content blocks
+  list.forEach((item, index) => {
+    const block = document.createElement("div");
+    block.classList.add("content-block");
+    if (index % 2 === 0) {
+      block.classList.add("a-block");
+    } else {
+      block.classList.add("b-block");
+    }
+
+    const textDiv = document.createElement("div");
+    textDiv.classList.add("block-text");
+    textDiv.innerHTML = `<h2>${item.title}</h2><p>${item.text}</p>`;
+
+    const imageDiv = document.createElement("div");
+    imageDiv.classList.add("block-image");
+    const image = document.createElement("img");
+    image.src = item.image;
+    image.alt = `${item.title} Image`;
+    image.style = "cursor: pointer;";
+    image.addEventListener("click", () => {
+      window.open(item.link);
+    });
+
+    if (index % 2 === 0) {
+      block.appendChild(imageDiv);
+    }
+
+    imageDiv.appendChild(image);
+    block.appendChild(textDiv);
+    if (index % 2 === 1) {
+      block.appendChild(imageDiv);
+    }
+    container.appendChild(block);
+  });
+}
+
+// Call the function to create content blocks
+createContentBlocks(work_experience);
+
+// Button event listeners to switch between states
+const all_states = [
+  {
+    state: document.getElementById("state1Btn"),
+    data: work_experience,
+  },
+  {
+    state: document.getElementById("state2Btn"),
+    data: game_development,
+  },
+  {
+    state: document.getElementById("state3Btn"),
+    data: creative_writing,
+  },
+];
+
+all_states[0].state.classList.add("active");
+
+// I know this is a nested loop but... come on
+for (let i = 0; i < all_states.length; i++) {
+  all_states[i].state.addEventListener("click", () => {
+    createContentBlocks(all_states[i].data);
+    for (let j = 0; j < all_states.length; j++) {
+      if (j === i) {
+        all_states[j].state.classList.add("active");
+      } else {
+        all_states[j].state.classList.remove("active");
+      }
+    }
+  });
+}

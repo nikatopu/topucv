d75ac84226e8d8710c4e9ee6dcef8428c62c2bfb
nikatopu <nikatopu@gmail.com>
2023-11-29 13:59:18 +0400

    Simplified Text
    Changed the text appearance so that it is more readable

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 const work_experience = [
   {
     title: "Social Media Manager at Career Home",
-    text: "I was in charge of handling multiple social media platforms, creating content, and carrying out plans to increase engagement and grow our online presence. Besides managing social media, I focused on building and maintaining customer relationships. I interacted with our audience, responding quickly to inquiries, comments, and messages to ensure high customer satisfaction and a positive brand image.<br><br>I also took on the responsibility of editing videos for social media. This included planning, editing, and producing engaging video content to showcase our products/services and connect with our audience effectively.<br><br>Additionally, I contributed to graphic design tasks as part of the overall content creation process. This involved creating visual elements for social media posts to maintain brand consistency and visual appeal across all platforms.",
+    text: "<b>1. Social Media Management and Content Creation</b> <p>&ensp; I played a key role in managing multiple social media platforms, overseeing content creation and implementing strategies to boost engagement and enhance our online presence.</p><b>2. Customer Relationship Building</b><p>&ensp; In addition to social media management, my focus extended to cultivating and sustaining strong customer relationships. I ensured prompt responses to inquiries, comments, and messages, aiming for high customer satisfaction and a favorable brand image.</p><b>3. Video Editing for Effective Storytelling</b><p>&ensp; Taking on the responsibility of video editing, I meticulously planned, edited, and produced compelling video content. These videos effectively showcased our products/services, fostering a deeper connection with our audience.</p><b>4. Graphic Design for Brand Consistency</b><p>&ensp; As part of the content creation process, I actively contributed to graphic design tasks. This encompassed the creation of visually appealing elements for social media posts, maintaining brand consistency and visual allure across all platforms</p>",
     image:
       "https://scontent.ftbs6-2.fna.fbcdn.net/v/t39.30808-6/376672486_177598988690070_1869829048044851372_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=MmUjtyMU_PsAX92NoP8&_nc_ht=scontent.ftbs6-2.fna&oh=00_AfDraxnxP_ZVAmUfSyl0JvT9ew6GHhFvrfbRkTNqBylJJQ&oe=65655C66",
     link: "https://www.facebook.com/profile.php?id=100093199086358",
   },
   {
     title: "Sales & Marketing Associate at Iberieli",
-    text: "I developed and maintained strong relationships with business associates to ensure ongoing collaboration and satisfaction. In my role, I directly sold wine to customers, using persuasive communication and product knowledge to drive sales and meet customer needs effectively. I played a significant part in expanding the market reach of the family business by targeting and engaging new audiences through strategic marketing initiatives.<br><br>Moreover, I served as a representative of Iberieli at various festivals, acting as a brand ambassador. This involved showcasing company products at international events, including a London-based exhibition, where I successfully promoted and represented the brand to a broader audience.",
+    text: "<b>1. Business Relationship Development</b><p>&ensp;I developed and maintained strong relationships with business associates to ensure ongoing collaboration and satisfaction.</p><b>2. Direct Sales and Customer Engagement</b><p>&ensp;In my role, I directly sold wine to customers, using persuasive communication and product knowledge to drive sales and meet customer needs effectively.</p><b>3. Market Expansion through Strategic Marketing</b><p>&ensp;I played a significant part in expanding the market reach of the family business by targeting and engaging new audiences through strategic marketing initiatives.</p><b>4. Brand Ambassador at International Events</b><p>&ensp;Moreover, I served as a representative of Iberieli at various festivals, acting as a brand ambassador. This involved showcasing company products at international events, including a London-based exhibition, where I successfully promoted and represented the brand to a broader audience.</p>",
     image:
       "https://scontent.ftbs6-2.fna.fbcdn.net/v/t39.30808-1/263399615_4922209917797553_6286832800449964494_n.jpg?stp=dst-jpg_p320x320&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=lswIsTNkcCUAX_y8ucZ&_nc_ht=scontent.ftbs6-2.fna&oh=00_AfAeLkXazgiA2JXHNkT2YmiIMPneV7A0OvajGtawf45LEQ&oe=656650B5",
     link: "https://www.facebook.com/iberieliwinerypage",
@@ -19,21 +19,21 @@ const work_experience = [
 game_development = [
   {
     title: '"CUBICAL"',
-    text: "This game, CUBICAL, is a significant achievement in my collection of work. While I am proud of creating it, I recognize the challenges that came with combining puzzle and action elements. <br><br> CUBICAL started as a fun project created for entertainment, using a 2D Game Engine called GameMaker. The main goal was to make a visually interesting game that looks three-dimensional, even though it was made within the limitations of a 2D Game Engine. <br><br> During this project, I gained valuable skills in creating complex puzzles and improved my ability to make 2D art and sound effects. The project was a great learning experience that gave me deep insights into the complex world of game development. It also helped me understand programming in the broader context of the industry. <br><br> CUBICAL's diverse nature allowed me to handle the challenges of both game development and programming, contributing to my overall growth and professionalism. The combination of creative problem-solving, artistic expression, and technical expertise displayed in this project reflects my commitment to continually improving and adapting in dynamic professional settings.",
+    text: "<b>1. Introduction to CUBICAL</b><p>&ensp;This game, CUBICAL, stands as a significant achievement in my collection of work. Despite the pride in its creation, I acknowledge the challenges faced in combining puzzle and action elements.</p><b>2. Origins and Development Process</b><p>&ensp;CUBICAL began as a fun project designed for entertainment, utilizing the GameMaker 2D Game Engine. The primary aim was to craft a visually intriguing game with a three-dimensional appearance, all within the constraints of a 2D Game Engine.</p><b>3. Skill Development and Learning Experience</b><p>&ensp;Throughout this project, I acquired valuable skills in creating intricate puzzles and enhanced my proficiency in 2D art and sound effects. The experience provided profound insights into the intricate world of game development and broadened my understanding of programming within the industry context.</p><b>4. Diverse Challenges and Professional Growth</b><p>&ensp;CUBICAL's diverse nature required me to navigate challenges in both game development and programming, contributing significantly to my overall growth and professionalism. The project showcased a blend of creative problem-solving, artistic expression, and technical expertise, reflecting my commitment to continuous improvement and adaptability in dynamic professional settings.</p",
     image:
       "https://img.itch.zone/aW1hZ2UvMTQ4OTIyNC84NjgyMDQzLnBuZw==/original/Rer00i.png",
     link: "https://nicolaeus.itch.io/cubical",
   },
   {
     title: '"Quit Horsing Around!"',
-    text: "While I may not take particular pride in it, I acknowledge the merits of the game, QUIT HORSING AROUND!, given the circumstances of its development.<br><br>Originating from a GameJam that utilized the 2D Game Engine, GameMaker, QUIT HORSING AROUND! was conceived within the constraints and thematic parameters of the event. I skillfully integrated the phrase 'Quit Horsing Around' into the core concept of this fast-paced, action-oriented game.<br><br>The progression of this project has significantly contributed to my professional development, especially in terms of employability. The process of creating QUIT HORSING AROUND! taught me to navigate effectively under stressful conditions and tight timelines, fostering the acquisition of invaluable skills. This endeavor serves as a testament to my adaptability and capacity to deliver results even in challenging circumstances, attributes that enhance my suitability as a potential employee in diverse work environments.",
+    text: "<b>1. Introduction to QUIT HORSING AROUND!</b><p>&ensp;While not a source of particular pride, I acknowledge the merits of the game, QUIT HORSING AROUND!, given the circumstances of its development.</p><b>2. Origin and Development Process</b><p>&ensp;Originating from a GameJam utilizing the GameMaker 2D Game Engine, QUIT HORSING AROUND! was conceived within the constraints and thematic parameters of the event. The core concept skillfully integrates the phrase 'Quit Horsing Around' into this fast-paced, action-oriented game.</p><b>3. Professional Development Impact</b><p>&ensp;The progression of this project has significantly contributed to my professional development, particularly in terms of employability. Creating QUIT HORSING AROUND! taught me to navigate effectively under stressful conditions and tight timelines, fostering the acquisition of invaluable skills.</p><b>4. Adaptability and Results-Oriented Approach</b><p>&ensp;This endeavor serves as a testament to my adaptability and capacity to deliver results even in challenging circumstances. These attributes enhance my suitability as a potential employee in diverse work environments.</p>",
     image:
       "https://img.itch.zone/aW1nLzEwNjYzOTE1LnBuZw==/315x250%23c/vnwy9p.png",
     link: "https://nicolaeus.itch.io/quit-horsing-around",
   },
   {
     title: '"The Robodventure"',
-    text: "This specific project, THE ROBODVENTURE, holds a significant place among my professional achievements. Despite my pride in its creation, a notable drawback is the excessive use of dialogue. <br><br> Conceived for a Christmas GameJam utilizing the 2D Game Engine, GameMaker, THE ROBODVENTURE was developed as part of a distinctive challenge. In this GameJam, I took on the role of a 'GameDev Santa', assigned the task of crafting a personalized game for a designated recipient. The individual provided preferences through a letter, guiding the ideation and subsequent development of the game concept.<br><br>The process of creating THE ROBODVENTURE played a crucial role in enhancing my qualifications as a potential candidate. It compelled me to explore uncharted aspects of game development, fostering innovation within the constraints of a limited time frame—merely one month. This experience not only highlighted my ability to adapt and deliver under challenging conditions but also demonstrated my capability to tailor creative solutions to meet specific preferences, a skill that is highly applicable in various professional contexts.",
+    text: "<b>1. Introduction to THE ROBODVENTURE</b><p>&ensp;This specific project, THE ROBODVENTURE, holds a significant place among my professional achievements. Despite my pride in its creation, a notable drawback is the excessive use of dialogue.</p><b>2. Origin and Development Process</b><p>&ensp;Conceived for a Christmas GameJam utilizing the GameMaker 2D Game Engine, THE ROBODVENTURE was developed as part of a distinctive challenge. In this GameJam, I took on the role of a 'GameDev Santa', tasked with crafting a personalized game for a designated recipient. The individual provided preferences through a letter, guiding the ideation and subsequent development of the game concept.</p><b>3. Impact on Professional Qualifications</b><p>&ensp;The process of creating THE ROBODVENTURE played a crucial role in enhancing my qualifications as a potential candidate. It compelled me to explore uncharted aspects of game development, fostering innovation within the constraints of a limited time frame—merely one month.</p><b>4. Adaptability and Tailored Solutions</b><p>&ensp;This experience not only highlighted my ability to adapt and deliver under challenging conditions but also demonstrated my capability to tailor creative solutions to meet specific preferences, a skill that is highly applicable in various professional contexts.</p>",
     image: "https://img.itch.zone/aW1nLzc3NDAxODIucG5n/original/eEWsz8.png",
     link: "https://nicolaeus.itch.io/the-robodventure",
   },
@@ -42,11 +42,28 @@ game_development = [
 creative_writing = [
   {
     title: "Poem - Let's Fly Far",
-    text: "I played a role in creating the poetic element of this animation, with my sister, Mariam Topuridze, handling the animation, and my brother, Ioane Topuridze, composing the musical accompaniment.<br><br>The inspiration for the poem stemmed from a straightforward video that highlighted the distinctions between two types of caterpillars. These caterpillars, though leading similar lives, undergo diverse transformations during their development.<br><br>Participating in the creation of this poem proved to be a mentally engaging pursuit. While the act of writing itself was conventional, the true fascination lay in the collaborative effort with my siblings to produce a collective work. Additionally, I took on the responsibility of translating the poem into English, a task I believe I carried out proficiently. <br><br>This experience has enriched my skill set and professional attributes in various ways. The collaboration with my siblings underscored my ability for teamwork and cooperation, highlighting my effectiveness in contributing to a collaborative creative process. The translation task further showcased my linguistic proficiency and attention to detail. Collectively, this multifaceted involvement demonstrates my adaptability, creativity, and meticulous approach to tasks, qualities that significantly enhance my capabilities as a work applicant.",
-    //Yes I'm using a discord photo stfu
+    text: "<b>1. Collaborative Creation in Animation</b><p>&ensp;I played a role in creating the poetic element of this animation, with my sister, Mariam Topuridze, handling the animation, and my brother, Ioane Topuridze, composing the musical accompaniment.</p><b>2. Inspiration from Caterpillar Transformations</b><p>&ensp;The inspiration for the poem stemmed from a straightforward video that highlighted the distinctions between two types of caterpillars. These caterpillars, though leading similar lives, undergo diverse transformations during their development.</p><b>3. Mentally Engaging Pursuit</b><p>&ensp;Participating in the creation of this poem proved to be a mentally engaging pursuit. While the act of writing itself was conventional, the true fascination lay in the collaborative effort with my siblings to produce a collective work. Additionally, I took on the responsibility of translating the poem into English, a task I believe I carried out proficiently.</p><b>4. Enriching Skills and Professional Attributes</b><p>&ensp;This experience has enriched my skill set and professional attributes in various ways. The collaboration with my siblings underscored my ability for teamwork and cooperation, highlighting my effectiveness in contributing to a collaborative creative process. The translation task further showcased my linguistic proficiency and attention to detail. Collectively, this multifaceted involvement demonstrates my adaptability, creativity, and meticulous approach to tasks, qualities that significantly enhance my capabilities as a work applicant.</p>",
+    //Yes I'm using a discord photo, because... why not
     image:
       "https://media.discordapp.net/attachments/435802394375225346/1178340298410172447/oh.png?ex=6575c9eb&is=656354eb&hm=32b5d7d027a8ce939f06c4559e7f76957ce1ce0fdc5d9b76b02c1a5e7d30b558&=&format=webp&width=927&height=663",
     link: "https://nikatopu.github.io/topuportfolio/fly%20far.mp4",
+  },
+];
+
+additional_skills = [
+  {
+    title: "",
+    text: "",
+    image: "",
+    link: "",
+  },
+];
+about_me = [
+  {
+    title: "",
+    text: "",
+    image: "",
+    link: "",
   },
 ];
 
@@ -67,7 +84,7 @@ function createContentBlocks(list) {
 
     const textDiv = document.createElement("div");
     textDiv.classList.add("block-text");
-    textDiv.innerHTML = `<h2>${item.title}</h2><p>${item.text}</p>`;
+    textDiv.innerHTML = `<h2>${item.title}</h2>${item.text}`;
 
     const imageDiv = document.createElement("div");
     imageDiv.classList.add("block-image");
@@ -108,6 +125,14 @@ const all_states = [
   {
     state: document.getElementById("state3Btn"),
     data: creative_writing,
+  },
+  {
+    state: document.getElementById("state4Btn"),
+    data: additional_skills,
+  },
+  {
+    state: document.getElementById("state5Btn"),
+    data: about_me,
   },
 ];
 

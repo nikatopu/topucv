d194de76e555a10ede220bdbf211240355b280d8
nikatopu <nikatopu@gmail.com>
2023-12-17 15:03:47 +0400

    JPG to WEBP
    Updated the photos so they load faster

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const work_experience = [
   {
     title: "Social Media Manager at Career Home",
     text: "<b>1. Social Media Management and Content Creation</b> <p>&ensp; I played a key role in managing multiple social media platforms, overseeing content creation and implementing strategies to boost engagement and enhance our online presence.</p><b>2. Customer Relationship Building</b><p>&ensp; In addition to social media management, my focus extended to cultivating and sustaining strong customer relationships. I ensured prompt responses to inquiries, comments, and messages, aiming for high customer satisfaction and a favorable brand image.</p><b>3. Video Editing for Effective Storytelling</b><p>&ensp; Taking on the responsibility of video editing, I meticulously planned, edited, and produced compelling video content. These videos effectively showcased our products/services, fostering a deeper connection with our audience.</p><b>4. Graphic Design for Brand Consistency</b><p>&ensp; As part of the content creation process, I actively contributed to graphic design tasks. This encompassed the creation of visually appealing elements for social media posts, maintaining brand consistency and visual allure across all platforms</p>",
-    image: "Photos/Career Home Sample.jpg",
+    image: "Photos/Career Home Sample.webp",
     link: "https://www.facebook.com/profile.php?id=100093199086358",
   },
   {
     title: "Sales & Marketing Associate at Iberieli",
     text: "<b>1. Business Relationship Development</b><p>&ensp;I developed and maintained strong relationships with business associates to ensure ongoing collaboration and satisfaction.</p><b>2. Direct Sales and Customer Engagement</b><p>&ensp;In my role, I directly sold wine to customers, using persuasive communication and product knowledge to drive sales and meet customer needs effectively.</p><b>3. Market Expansion through Strategic Marketing</b><p>&ensp;I played a significant part in expanding the market reach of the family business by targeting and engaging new audiences through strategic marketing initiatives.</p><b>4. Brand Ambassador at International Events</b><p>&ensp;Moreover, I served as a representative of Iberieli at various festivals, acting as a brand ambassador. This involved showcasing company products at international events, including a London-based exhibition, where I successfully promoted and represented the brand to a broader audience.</p>",
-    image: "Photos/Iberieli.jpg",
+    image: "Photos/Iberieli.webp",
     link: "https://www.facebook.com/iberieliwinerypage",
   },
 ];
@@ -18,21 +18,19 @@ game_development = [
   {
     title: '"CUBICAL"',
     text: "<b>1. Introduction to CUBICAL</b><p>&ensp;This game, CUBICAL, stands as a significant achievement in my collection of work. Despite the pride in its creation, I acknowledge the challenges faced in combining puzzle and action elements.</p><b>2. Origins and Development Process</b><p>&ensp;CUBICAL began as a fun project designed for entertainment, utilizing the GameMaker 2D Game Engine. The primary aim was to craft a visually intriguing game with a three-dimensional appearance, all within the constraints of a 2D Game Engine.</p><b>3. Skill Development and Learning Experience</b><p>&ensp;Throughout this project, I acquired valuable skills in creating intricate puzzles and enhanced my proficiency in 2D art and sound effects. The experience provided profound insights into the intricate world of game development and broadened my understanding of programming within the industry context.</p><b>4. Diverse Challenges and Professional Growth</b><p>&ensp;CUBICAL's diverse nature required me to navigate challenges in both game development and programming, contributing significantly to my overall growth and professionalism. The project showcased a blend of creative problem-solving, artistic expression, and technical expertise, reflecting my commitment to continuous improvement and adaptability in dynamic professional settings.</p",
-    image:
-      "https://img.itch.zone/aW1hZ2UvMTQ4OTIyNC84NjgyMDQzLnBuZw==/original/Rer00i.png",
+    image: "Photos/cubical.webp",
     link: "https://nicolaeus.itch.io/cubical",
   },
   {
     title: '"Quit Horsing Around!"',
     text: "<b>1. Introduction to QUIT HORSING AROUND!</b><p>&ensp;While not a source of particular pride, I acknowledge the merits of the game, QUIT HORSING AROUND!, given the circumstances of its development.</p><b>2. Origin and Development Process</b><p>&ensp;Originating from a GameJam utilizing the GameMaker 2D Game Engine, QUIT HORSING AROUND! was conceived within the constraints and thematic parameters of the event. The core concept skillfully integrates the phrase 'Quit Horsing Around' into this fast-paced, action-oriented game.</p><b>3. Professional Development Impact</b><p>&ensp;The progression of this project has significantly contributed to my professional development, particularly in terms of employability. Creating QUIT HORSING AROUND! taught me to navigate effectively under stressful conditions and tight timelines, fostering the acquisition of invaluable skills.</p><b>4. Adaptability and Results-Oriented Approach</b><p>&ensp;This endeavor serves as a testament to my adaptability and capacity to deliver results even in challenging circumstances. These attributes enhance my suitability as a potential employee in diverse work environments.</p>",
-    image:
-      "https://img.itch.zone/aW1nLzEwNjYzOTE1LnBuZw==/315x250%23c/vnwy9p.png",
+    image: "Photos/horsing around.webp",
     link: "https://nicolaeus.itch.io/quit-horsing-around",
   },
   {
     title: '"The Robodventure"',
     text: "<b>1. Introduction to THE ROBODVENTURE</b><p>&ensp;This specific project, THE ROBODVENTURE, holds a significant place among my professional achievements. Despite my pride in its creation, a notable drawback is the excessive use of dialogue.</p><b>2. Origin and Development Process</b><p>&ensp;Conceived for a Christmas GameJam utilizing the GameMaker 2D Game Engine, THE ROBODVENTURE was developed as part of a distinctive challenge. In this GameJam, I took on the role of a 'GameDev Santa', tasked with crafting a personalized game for a designated recipient. The individual provided preferences through a letter, guiding the ideation and subsequent development of the game concept.</p><b>3. Impact on Professional Qualifications</b><p>&ensp;The process of creating THE ROBODVENTURE played a crucial role in enhancing my qualifications as a potential candidate. It compelled me to explore uncharted aspects of game development, fostering innovation within the constraints of a limited time frame—merely one month.</p><b>4. Adaptability and Tailored Solutions</b><p>&ensp;This experience not only highlighted my ability to adapt and deliver under challenging conditions but also demonstrated my capability to tailor creative solutions to meet specific preferences, a skill that is highly applicable in various professional contexts.</p>",
-    image: "https://img.itch.zone/aW1nLzc3NDAxODIucG5n/original/eEWsz8.png",
+    image: "Photos/robodventure.webp",
     link: "https://nicolaeus.itch.io/the-robodventure",
   },
 ];
@@ -42,7 +40,7 @@ creative_writing = [
     title: "Poem - Let's Fly Far",
     text: "<b>1. Collaborative Creation in Animation</b><p>&ensp;I played a role in creating the poetic element of this animation, with my sister, Mariam Topuridze, handling the animation, and my brother, Ioane Topuridze, composing the musical accompaniment.</p><b>2. Inspiration from Caterpillar Transformations</b><p>&ensp;The inspiration for the poem stemmed from a straightforward video that highlighted the distinctions between two types of caterpillars. These caterpillars, though leading similar lives, undergo diverse transformations during their development.</p><b>3. Mentally Engaging Pursuit</b><p>&ensp;Participating in the creation of this poem proved to be a mentally engaging pursuit. While the act of writing itself was conventional, the true fascination lay in the collaborative effort with my siblings to produce a collective work. Additionally, I took on the responsibility of translating the poem into English, a task I believe I carried out proficiently.</p><b>4. Enriching Skills and Professional Attributes</b><p>&ensp;This experience has enriched my skill set and professional attributes in various ways. The collaboration with my siblings underscored my ability for teamwork and cooperation, highlighting my effectiveness in contributing to a collaborative creative process. The translation task further showcased my linguistic proficiency and attention to detail. Collectively, this multifaceted involvement demonstrates my adaptability, creativity, and meticulous approach to tasks, qualities that significantly enhance my capabilities as a work applicant.</p>",
     //Yes I'm using a discord photo, because... why not
-    image: "Photos/fly far.jpg",
+    image: "Photos/fly far.webp",
     link: "https://nikatopu.github.io/topuportfolio/fly%20far.mp4",
   },
 ];
@@ -51,13 +49,13 @@ additional_skills = [
   {
     title: "Microsoft Office",
     text: "<b>1. Microsoft Word</b><p>&ensp;My proficiency with Microsoft Word extends beyond personal use to professional applications. Over the past 5 years, I've garnered extensive experience, acquiring nuanced skills and employing various tricks. Notably, I contributed to the creation and design of blogs for Career Home.</p><b>2. Microsoft PowerPoint</b><p>&ensp;I possess significant expertise in Microsoft PowerPoint, having developed numerous professional-level slideshows. These presentations serve not only personal needs but also contribute to major university events like Podcasts. My adeptness with the latest version of PowerPoint allows me to utilize a wide range of features effectively.</p><b>3. Microsoft Excel</b><p>&ensp;While I have limited experience with Microsoft Excel, I've predominantly relied on alternative tools like Google Sheets for professional tasks. Despite my beginner-level skills, I bring value through my proficiency, proving to be a useful contributor.</p>",
-    image: "Photos/Microsoft Office.jpg",
+    image: "Photos/Microsoft Office.webp",
     link: "",
   },
   {
     title: "Adobe Products",
     text: "<b>1. Adobe Photoshop</b><p>&ensp;In the realm of graphic design, I predominantly utilized free alternatives to Adobe Photoshop, such as paint.net and Krita, for crafting straightforward designs. Nevertheless, I possess a degree of proficiency in using Adobe Photoshop itself.</p><b>2. Adobe Illustrator</b><p>&ensp;My engagement with vector-based designs has been primarily for personal applications, providing me with a modest level of familiarity with Adobe Illustrator.</p><b>3. Adobe Premiere Pro</b><p>&ensp;While my usage of alternatives like Davinci Resolve and VSDC has been more frequent, I gained substantial professional experience in video editing through my role at Career Home (refer to Work Experience page), demonstrating competence in Adobe Premiere Pro.</p>",
-    image: "Photos/Adobe LOGO.jpg",
+    image: "Photos/Adobe LOGO.webp",
     link: "",
   },
 ];

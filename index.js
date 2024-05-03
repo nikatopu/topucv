a4b0cc9a857178189e3b70c7f191edeff156486b
nikatopu <nikatopu@gmail.com>
2024-05-03 13:08:37 +0400

    Updated portfolio to NodeJS
    Now we're talking

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+var __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname + "/public"));
+app.set('views', __dirname + '/public');
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    res.render("main.ejs");
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+})
